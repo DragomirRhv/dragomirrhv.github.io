be092ded83e8fd6f9f61ccd92ba3c108833b8817
Dragomir Ryahov <dragomir.ryahov@altscale.com>
2021-01-09 01:05:23 +0200

    else statement added in ToDo List to prevent creating an empty task list

--- assets/projects/VanillaJSProjects/ToDoList/app.js
+++ assets/projects/VanillaJSProjects/ToDoList/app.js
@@ -29,13 +29,13 @@ function loadEventListeners() {
 // Get Tasks from LS
 function getTasks() {
     let tasks;
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
 
-    tasks.forEach(function(task) {
+    tasks.forEach(function (task) {
         // Create li element
         const li = document.createElement('li');
         // Add class
@@ -59,30 +59,30 @@ function getTasks() {
 
 // Add Task
 function addTask(e) {
-    if(taskInput.value === '') {
+    if (taskInput.value === '') {
         alert('Add a task');
+    } else {
+        // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value))
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append the link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+
+        // Store in LS
+        storeTaskInLocalStorage(taskInput.value);
     }
-
-    // Create li element
-    const li = document.createElement('li');
-    // Add class
-    li.className = 'collection-item';
-    // Create text node and append to li
-    li.appendChild(document.createTextNode(taskInput.value))
-    // Create new link element
-    const link = document.createElement('a');
-    // Add class
-    link.className = 'delete-item secondary-content';
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>';
-    // Append the link to li
-    li.appendChild(link);
-
-    // Append li to ul
-    taskList.appendChild(li);
-
-    // Store in LS
-    storeTaskInLocalStorage(taskInput.value);
 
     // Clear input
     taskInput.value = '';
@@ -92,7 +92,7 @@ function addTask(e) {
 // Store Task
 function storeTaskInLocalStorage(task) {
     let tasks;
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -104,8 +104,8 @@ function storeTaskInLocalStorage(task) {
 
 // Remove Task
 function removeTask(e) {
-    if(e.target.parentElement.classList.contains('delete-item')) {
-        if(confirm('Are You Sure?')) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are You Sure?')) {
             e.target.parentElement.parentElement.remove();
 
             // Remove from LS
@@ -117,14 +117,14 @@ function removeTask(e) {
 // Remove from LS
 function removeTaskFromLocalStorage(taskitem) {
     let tasks;
-    if(localStorage.getItem('tasks') === null) {
+    if (localStorage.getItem('tasks') === null) {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem('tasks'));
     }
 
-    tasks.forEach(function(task, index){
-        if(taskitem.textContent === task) {
+    tasks.forEach(function (task, index) {
+        if (taskitem.textContent === task) {
             tasks.splice(index, 1);
         }
     })
@@ -137,7 +137,7 @@ function clearTasks() {
     // taskList.innerHTML = '';
 
     // Faster
-    while(taskList.firstChild) {
+    while (taskList.firstChild) {
         taskList.removeChild(taskList.firstChild);
     }
 
@@ -153,11 +153,11 @@ function clearTasksFromLocalStorage() {
 function filterTasks(e) {
     const text = e.target.value.toLowerCase();
 
-    document.querySelectorAll('.collection-item').forEach(function(task) {
+    document.querySelectorAll('.collection-item').forEach(function (task) {
         const item = task.firstChild.textContent;
-        if(item.toLowerCase().indexOf(text) != -1){
+        if (item.toLowerCase().indexOf(text) != -1) {
             task.style.display = 'block';
-        }else {
+        } else {
             task.style.display = 'none';
         }
     })
